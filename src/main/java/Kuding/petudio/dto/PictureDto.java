b7bfdbdc383b184735b780fffaee2e0b548bb40c
jslee99 <junsub_lee@naver.com>
2023-09-22 16:31:00 +0900

    init

---
+++ src/main/java/Kuding/petudio/dto/PictureDto.java
@@ -0,0 +1,10 @@
+package Kuding.petudio.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class PictureDto {
+    private String originalName;
+    private MultipartFile file;
+}
