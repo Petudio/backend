e996ccd6159573efe078be81230d45a30ffcd23e
DDongYul <comeme0101@naver.com>
2023-09-22 14:43:19 +0900

    initial commit

---
+++ src/main/java/Kuding/petudio/PetudioApplication.java
@@ -0,0 +1,13 @@
+package Kuding.petudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetudioApplication.class, args);
+	}
+
+}
