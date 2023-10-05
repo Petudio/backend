865c5ffd467375dbd5f08d846f2f6f8d8933dfc3
DDongYul <comeme0101@naver.com>
2023-10-05 15:51:28 +0900

    Refactor: 불필요한 코드 삭제, 주석 추가

--- src/main/java/kuding/petudio/controller/BundleController.java
+++ src/main/java/kuding/petudio/controller/BundleController.java
@@ -1,18 +1,13 @@
 package kuding.petudio.controller;
 
 import kuding.petudio.controller.dto.BundleUploadDTO;
-import kuding.petudio.domain.BundleType;
-import kuding.petudio.domain.Picture;
 import kuding.petudio.domain.PictureType;
-import kuding.petudio.service.AmazonService;
 import kuding.petudio.service.BundleService;
 import kuding.petudio.service.PictureService;
 import kuding.petudio.service.dto.ServiceParamPictureDto;
 import kuding.petudio.service.dto.ServiceReturnBundleDto;
-import kuding.petudio.service.dto.ServiceReturnPictureDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,20 +25,17 @@ public class BundleController {
 
     private final BundleService bundleService;
     private final PictureService pictureService;
-    private final AmazonService amazonService;
 
+    /**
+     * Bundle 목록 조회
+     * @return JSON 형식 데이터
+     */
     @GetMapping
     @ResponseBody
     public List<ServiceReturnBundleDto> bundleList(
             @RequestParam int pageOffset,
             @RequestParam int pageSize) {
         List<ServiceReturnBundleDto> recentBundles = bundleService.findRecentBundles(pageOffset, pageSize);
-
-//        ServiceReturnPictureDto testDTO = new ServiceReturnPictureDto(1L, "test", new byte['1'], PictureType.BEFORE);
-//        List<ServiceReturnPictureDto> serviceReturnPictureDtos = new ArrayList<>();
-//        serviceReturnPictureDtos.add(testDTO);
-//        ServiceReturnBundleDto testData = new ServiceReturnBundleDto(1L, serviceReturnPictureDtos, ANIMAL_TO_HUMAN);
-//        recentBundles.add(testData); //테스트용
         return recentBundles;
     }
 
@@ -62,24 +54,6 @@ public class BundleController {
         return pictureDtos;
     }
 
-//    /**
-//     * 생성 버튼 눌렀을 때 -> 프론트에서 처리
-//     * 1.before 이미지가 저장되어 있는 Bundle을 조회
-//     * 2.Picture 서비스의 AI 메서드를 통해 afterImage 생성
-//     * 3.조회한 번들에 afterImage 정보를 담아 다시 DB에 저장
-//     */
-//    @PostMapping("/upload/{bundleId}")
-//    public void makeAfterImage(@PathVariable Long bundleId) {
-//        ServiceReturnBundleDto findBundle = bundleService.findBundleById(bundleId); //번들에서 before 이미지가 저장되어 있는 번들 조회
-//        List<ServiceReturnPictureDto> bundlePictures = findBundle.getPictures(); //조회한 번들에서 이미지 정보 가져옴
-//        ServiceReturnPictureDto returnBeforePicture = bundlePictures.get(0); //before 이미지 정보
-//        MultipartFile multipartFile = amazonService.convertByteArrayToMultiFile(returnBeforePicture.getPictureByteArray());
-//        ServiceParamPictureDto beforePicture = new ServiceParamPictureDto(returnBeforePicture.getOriginalName(), multipartFile, returnBeforePicture.getPictureType());
-//        ServiceParamPictureDto afterPicture = pictureService.animalToHuman(beforePicture);
-//
-//    }
-
-
     /**
      * 프론트에서 두 사진에 대한 DTO를 받아 DB에 저장한다.
      * @param bundleUploadDTO -> JSON 형식 HTTPBody
@@ -92,11 +66,6 @@ public class BundleController {
         String bundleTitle = bundleUploadDTO.getBundleTitle();
 
         bundleService.saveBundleBindingPictures(pictureDtos, bundleTitle, ANIMAL_TO_HUMAN); // 게시글 업로드
-    }
-
-    private ServiceParamPictureDto makeServiceParamPictureDto(MultipartFile multipartFile, PictureType pictureType) {
-        String originalName = multipartFile.getOriginalFilename();
-        return new ServiceParamPictureDto(originalName, multipartFile, pictureType);
     }
 
     @PostMapping("/{bundleId}/like")
