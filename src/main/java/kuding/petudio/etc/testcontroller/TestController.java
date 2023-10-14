576ab68ff9c1d04197d22775f457c972dda05505
DDongYul <comeme0101@naver.com>
2023-10-14 22:37:44 +0900

    Test: Rest Template을 통해 MultipartFile 전송 테스트 구현

--- src/main/java/kuding/petudio/etc/testcontroller/TestController.java
+++ src/main/java/kuding/petudio/etc/testcontroller/TestController.java
@@ -1,5 +1,8 @@
 package kuding.petudio.etc.testcontroller;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kuding.petudio.domain.Bundle;
 import kuding.petudio.domain.BundleType;
 import kuding.petudio.domain.PictureType;
@@ -10,10 +13,21 @@ import kuding.petudio.service.dto.ServiceParamPictureDto;
 import kuding.petudio.service.dto.ServiceReturnBundleDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -27,8 +41,10 @@ public class TestController {
     @Autowired
     private AiPictureService aiPictureService;
 
+
     /**
      * ai 생성 모델에 대한 샘플 컨트롤러
+     *
      * @param picture1
      * @return
      */
@@ -56,4 +72,35 @@ public class TestController {
         log.info("bundle size = {}", recentPublicBundles.size());
         return recentPublicBundles;
     }
+
+    @PostMapping("/test/aiserver")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        String serverUrl = "http://localhost:8081/upload";
+
+        ByteArrayResource fileResource = new ByteArrayResource(file.getBytes()) {
+            // 기존 ByteArrayResource의 getFilename 메서드 override
+            @Override
+            public String getFilename() {
+                return "beforePicture";
+            }
+        };
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", fileResource);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                serverUrl,
+                HttpMethod.POST,
+                requestEntity,
+                String.class);
+        return "ok";
+    }
 }
+
