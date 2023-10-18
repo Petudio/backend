94d55d789a9e68c6f2e9f91bfa1abd4f371795c4
jslee99 <junsub_lee@naver.com>
2023-10-18 16:01:38 +0900

    AiServerCallService 리팩토링

--- src/main/java/kuding/petudio/domain/BundleType.java
+++ src/main/java/kuding/petudio/domain/BundleType.java
@@ -4,5 +4,18 @@ package kuding.petudio.domain;
  * AI 모델중에서 어떠한 AI 모델을 사용했는지 기록
  */
 public enum BundleType {
-    ANIMAL_TO_HUMAN, FOUR_AI_PICTURES, COPY, SERVICE4
+    ANIMAL_TO_HUMAN("/animal-to-human/upload"),
+    FOUR_AI_PICTURES("/four-cuts/upload"),
+    COPY("/copy/upload"),
+    SERVICE4("/service4/upload");
+
+    private final String aiServerPathUrl;
+
+    BundleType(String aiServerPathUrl) {
+        this.aiServerPathUrl = aiServerPathUrl;
+    }
+
+    public String getAiServerPathUrl() {
+        return this.aiServerPathUrl;
+    }
 }
