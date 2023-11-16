38d708fe8ca3774bb74df17a23f0679eb1b3686c
jslee99 <junsub_lee@naver.com>
2023-11-16 11:21:46 +0900

    prompt 수정

--- src/main/java/kuding/petudio/service/PromptService.java
+++ src/main/java/kuding/petudio/service/PromptService.java
@@ -74,7 +74,7 @@ public class PromptService {
         promptConverter.put("모자", "hat");
         promptConverter.put("선글라스", "sun glass");
         promptConverter.put("수영", "swimming pool");
-        promptConverter.put("우주", "sakura, sakura petals");
+        promptConverter.put("벚꽃", "sakura, sakura petals");
         promptConverter.put("꽃밭", "flower garden");
         promptConverter.put("피라미드", "pyramid");
         promptConverter.put("동화", "fairy tale");
